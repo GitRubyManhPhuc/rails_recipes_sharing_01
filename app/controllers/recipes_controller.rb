03f1933dc3579456f4fd9ab494922feff5a81263
GitRubyManhPhuc <thachnguyenhl@gmail.com>
2018-08-06 15:07:10 +0700

    views count and callback

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
-  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, except: [:show, :index]
+  impressionist actions: [:show]
 
   def index
     @recipes = Recipe.desc.page(params[:page])
@@ -9,13 +10,12 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
     @categories = Category.all
-    @recipe.steps.build
-    @recipe.recipe_ingredients.build
+    3.times{@recipe.steps.build}
+    3.times{@recipe.recipe_ingredients.build}
   end
 
   def create
     @recipe = Recipe.new recipe_params
-    @recipe.category_ids = params[:recipe][:category_ids]
     if @recipe.save
       redirect_to @recipe
     else
@@ -24,41 +24,25 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find_by id: params[:id]
     @user = @recipe.user
-    @favor = Favor.rela current_user.id, @recipe
-    @bookmark = Favor.rela_bookmark current_user.id, @recipe
     @comments = @recipe.comments.all
     @comment = @recipe.comments.build
-  end
-
-  def edit; end
-
-  def update
-    if @recipe.update_attributes recipe_params
-      flash[:success] = t ".save_success"
-      redirect_to @recipe
-    else
-      flash[:danger] = t ".save_failed"
-      render :edit
-    end
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :cover_photo,
-      :description, :purpose, :ready_in, :difficult_level, :category_ids,
-      :people_num, steps_attributes: [:id, :content, :_destroy, {photos: []}],
-      recipe_ingredients_attributes: [:id, :name, :amount, :measurement])
-          .merge user_id: current_user.id
+    params.require(:recipe).permit(:name,
+      :description, :purpose, :ready_in, :difficult_level,
+      :people_num, steps_attributes: [:content],
+      recipe_ingredients_attributes: [:temp]).merge user_id: current_user.id
   end
 
-  def find_recipe
-    @recipe = Recipe.find_by id: params[:id]
-
-    return if @recipe
-    flash[:danger] = t ".cant_find"
-    redirect_to root_path
-
+  def logged_in_user
+    return if logged_in?
+    flash[:danger] = t "please_login"
+    redirect_to login_url
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   before_action :find_user, except: [:new, :create, :index]
   before_action :correct_user, except: [:show, :index]
 
